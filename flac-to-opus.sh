5ed30e8c4f252f71bba1b199019e9d24748ab73e
Nick Kenworthy <xndt98@live.com>
2021-07-18 12:37:09 -0700

    convert cue files that come with flac

--- flac-to-opus.sh
+++ flac-to-opus.sh
@@ -17,3 +17,8 @@ find "$OPUS_PATH" -name "*.flac" -type f -exec rm '{}' \;
 
 # Removes the ".flac" substring from all the "*.flac.opus" files
 find "$OPUS_PATH" -name "*.flac.opus" -type f -exec rename '.flac' '' '{}' \;
+
+# Update any ".cue" files to have "opus" instead of "flac"
+# Only necessary if the flac files are accompanied with
+# cue files specifying different tracks in a CD
+find "$OPUS_PATH" -name "*.cue" -exec sed -i 's/.flac/.opus/g' {} \;
