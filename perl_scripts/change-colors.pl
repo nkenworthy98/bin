85710f8e9578389ed6350404acf8963ade31ff98
Nick Kenworthy <xndt98@live.com>
2021-09-13 20:47:15 -0700

    add change dmenu colors
    remove comments/debug statements

--- perl_scripts/change-colors.pl
+++ perl_scripts/change-colors.pl
@@ -3,20 +3,18 @@
 use strict;
 use warnings;
 
-# my $new_color = $ENV{'PROMPT_MAIN_COLOR'};
-my $new_color = '#112233';
+my $new_color = $ENV{'MAIN_CUSTOM_COLOR'};
 my $home = $ENV{'HOME'};
-my $path_zshrc = "$home/.zshrc";
-my $path_tmux_conf = "$home/tmux.conf.test";
+my $path_tmux_conf = "$home/.tmux.conf";
 # The paths for the suckless programs expect to have a trailing "/"
 my $path_dwm = "$home/.sucklessPrograms/dwm/";
+my $path_dmenu = "$home/.sucklessPrograms/dmenu/";
 
 is_hex($new_color) or die "ERROR: $new_color isn't a hexadecimal number";
 
-change_zshrc_colors($new_color, $path_zshrc);
-# change_tmux_colors($new_color, $path_tmux_conf);
-# change_dwm_colors($new_color, $path_dwm);
-
+change_tmux_colors($new_color, $path_tmux_conf);
+change_dwm_colors($new_color, $path_dwm);
+change_dmenu_colors($new_color, $path_dmenu);
 
 sub is_hex {
   my $num = shift @_;
@@ -26,10 +24,6 @@ sub is_hex {
   } else {
     return 0;
   }
-}
-
-sub change_zshrc_colors {
-  my ($color, $path_conf) = @_;
 }
 
 sub change_tmux_colors {
@@ -45,7 +39,7 @@ sub change_tmux_colors {
         || /set-option -g message-style fg='#[a-fA-F0-9]{6}'/
         || /set-option -g status-style fg='#[a-fA-f0-9]{6}'/
         || /set -g status-right "#\[fg=#[a-fA-F0-9]{6}\]%A, %d %b %Y %I:%M %p"/) {
-      s/#[a-fA-f0-9]{6}/$new_color/;
+      s/#[a-fA-f0-9]{6}/$color/;
     }
     push(@tmux_contents, $_);
   }
@@ -63,9 +57,6 @@ sub change_dwm_colors {
   my ($color, $path) = @_;
   my $path_conf = $path . "config.h";
 
-  print "Here's the path: $path\n";
-  print "Here's the path to the config: $path_conf\n";
-
   open(my $dwm_in, "<", $path_conf) or die "Can't open $path_conf: $!";
 
   my @dwm_contents;
@@ -73,7 +64,7 @@ sub change_dwm_colors {
   # Substitute the line with necessary change
   while (<$dwm_in>) {
     if (/static const char col_cyan\[\]\s+= "#[a-fA-F0-9]{6}";/) {
-      s/#[a-fA-F0-9]{6}/$new_color/;
+      s/#[a-fA-F0-9]{6}/$color/;
     }
     push(@dwm_contents, $_);
   }
@@ -88,9 +79,36 @@ sub change_dwm_colors {
 
   # Compile dwm with changes
   chdir $path;
+  my $make_output = `make`;
+  print $make_output;
+}
 
-  # I don't think this will show STDERR
-  # Look for another solution if I have a problem
+sub change_dmenu_colors {
+  my ($color, $path) = @_;
+  my $path_conf = $path . "config.h";
+
+  open(my $dmenu_in, "<", $path_conf) or die "Can't open $path_conf: $!";
+
+  my @dmenu_contents;
+
+  # Substitute the line with necessary change
+  while (<$dmenu_in>) {
+    if (/\[SchemeSel\] = \{ "#ffffff", "(?<select_color>#[a-fA-F0-9]{6})" \}/) {
+      s/$+{select_color}/$color/;
+    }
+    push(@dmenu_contents, $_);
+  }
+  close $dmenu_in or die "$dmenu_in: $!";
+
+  # Write all the lines to the same file
+  open(my $dmenu_out, ">", $path_conf);
+  foreach (@dmenu_contents) {
+    print $dmenu_out $_;
+  }
+  close $dmenu_out or die "$dmenu_out: $!";
+
+  # Compile dmenu with changes
+  chdir $path;
   my $make_output = `make`;
   print $make_output;
 }
