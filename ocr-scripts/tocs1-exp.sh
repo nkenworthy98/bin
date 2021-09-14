3a2cd532900585b6725694734c159b23b16afed8
Nick Kenworthy <xndt98@live.com>
2021-09-13 23:37:37 -0700

    add ocr-scripts (tocs-exp and tocs-mq-exp)

---
+++ ocr-scripts/tocs1-exp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Script to quickly grab current EXP and EXP until next level in
+# The Legend of Heroes: Trails of Cold Steel. Once these EXP values
+# are captured, they are added together and then subtracted by 1, so I
+# could plug that value into a memory editor (gameconqueror) to raise my characters levels
+# quickly. In the game, if you try to raise your EXP value too high,
+# your EXP seems to go down. The best thing to do seems to be to change your
+# current EXP value to 1 below the EXP needed in order to level up.
+set -euo pipefail
+
+# The image is scaled up, turned to black and white, and then the colors are inverted because black on white background seems
+# to work better than white on black background
+TEXT_OUTPUT=$(maim -g 144x61+2231+487 | convert +dither -colors 3 -colors 2 -colorspace gray -normalize -scale 300% -channel RGB -negate - - | tesseract --dpi 300 - - | perl -pe 's/\n/ /g' | awk '{print $1+$2-1}')
+
+# Output value to screen as a notification and send to clipboard
+notify-send "$TEXT_OUTPUT"
+echo -n "$TEXT_OUTPUT" | xclip -selection c
