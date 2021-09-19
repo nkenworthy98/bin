546a8f8224e8283d52475a13a39290f5c209fa3f
Nick Kenworthy <xndt98@live.com>
2021-09-18 22:56:45 -0700

    clean up mpcStatus.sh

--- mpcStatus.sh
+++ mpcStatus.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Script to display mpc status information through dunst
 
-status=$(mpc status)
-
-notify-send "$status"
-# echo -e "$status" | timeout 4s dmenu -l 3 -p "Current Song: " dunst
-
-exit
+notify-send "$(mpc status)"
