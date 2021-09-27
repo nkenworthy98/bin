e083ba35372797b656c522b915f327723b067b37
Nick Kenworthy <xndt98@live.com>
2021-09-26 23:49:07 -0700

    add var to specify path to invidious json

--- grii.sh
+++ grii.sh
@@ -5,5 +5,7 @@
 # latest invidious instances data
 set -euo pipefail
 
+INVIDIOUS_JSON_PATH="$HOME/bin/txt-files/invidious.json"
+
 # Grab a random invidious instance where the 30dRatio is greater than 95
-jq -r '.[][1] | select(.monitor."30dRatio" > 95) | .uri' txt-files/invidious.json | shuf -n 1
+jq -r '.[][1] | select(.monitor."30dRatio" > 95) | .uri' "$INVIDIOUS_JSON_PATH" | shuf -n 1
