389152ee199c458fad5c650ad9be8f39a1e1e331
Nick Kenworthy <xndt98@live.com>
2021-09-27 00:02:38 -0700

    remove https from random instance
    https is added in in convert-links.sh, so
    it needs to be removed here

--- grii.sh
+++ grii.sh
@@ -8,4 +8,5 @@ set -euo pipefail
 INVIDIOUS_JSON_PATH="$HOME/bin/txt-files/invidious.json"
 
 # Grab a random invidious instance where the 30dRatio is greater than 95
-jq -r '.[][1] | select(.monitor."30dRatio" > 95) | .uri' "$INVIDIOUS_JSON_PATH" | shuf -n 1
+# Also, get rid of https in link because convert-links.sh adds it back in
+jq -r '.[][1] | select(.monitor."30dRatio" > 95) | .uri' "$INVIDIOUS_JSON_PATH" | grep -v "vid\." | sed 's#https://##g' | shuf -n 1
