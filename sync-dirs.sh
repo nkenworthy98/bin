f20339c224634d0e33982079810e6499ac3c1255
Nick Kenworthy <xndt98@live.com>
2021-09-26 21:38:44 -0700

    rewrite sync-dirs to use different txt files
    Combine functionality of sync-dirs and large-sync-dirs because the only
    difference between the two was which text file was being used. This
    rewrite allows the user to use any txt file containing directories they
    want synced by passing that file as an argument when running the command.

--- sync-dirs.sh
+++ sync-dirs.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This is the text file containing all the directories that should be synced
+FILES_TO_SYNC="$1"
+
 # Complete change from previous rsync script
 # Sync to artix-desktop
 ssh -o ConnectTimeout=3 nickartix@artix-desktop exit && \
-    rsync -arvzP --files-from=bin/files-to-sync.txt ~/ nickartix@artix-desktop:~/
+    rsync -arvzP --files-from="$FILES_TO_SYNC" ~/ nickartix@artix-desktop:~/
 
 # Sync to manjaro-ideapad
 ssh -o ConnectTimeout=3 nick@manjaro-ideapad exit && \
-    rsync -arvzP --files-from=bin/files-to-sync.txt ~/ nick@manjaro-ideapad:~/
+    rsync -arvzP --files-from="$FILES_TO_SYNC" ~/ nick@manjaro-ideapad:~/
 
 # Checks to see if encrypted drive is connected and mounted
 ssh -o ConnectTimeout=3 nick-pi@nickpi '[ -d ~/mountDir/nick-storage/ ]' && \
-    rsync -arvzP --files-from=bin/files-to-sync.txt ~/ nick-pi@nickpi:~/mountDir/nick-storage/
+    rsync -arvzP --files-from="$FILES_TO_SYNC" ~/ nick-pi@nickpi:~/mountDir/nick-storage/
