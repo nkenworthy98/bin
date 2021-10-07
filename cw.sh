1b5df3cc34a3f95766c686684b908c3b485b6081
Nick Kenworthy <xndt98@live.com>
2021-10-07 00:23:11 -0700

    rewrite cw.sh to be POSIX compliant

--- cw.sh
+++ cw.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Script to change wallpaper. This script
 # takes in the first argument of cw.sh
 # and copies it to ~/.config/wallpaper.jpg, which
 # is what I'm using to be the default wallpaper.
 # My xinitrc takes this file and sets it using xwallpaper.
-set -euo pipefail
 
 cp "$1" ~/.config/wallpaper.jpg
