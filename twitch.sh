5a72f957e71c9f980e1778ddd830375b68d0bf95
Nick Kenworthy <xndt98@live.com>
2021-10-07 01:38:49 -0700

    rewrite twitch.sh to be POSIX compliant
    remove old comments for twitchy
    
    fix variable naming conventions

--- twitch.sh
+++ twitch.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 # Script to choose twitch stream through dmenu
 
-#choices=$(cat ~/bin/currentlyLive.txt)
-#choices=$(twitchy --non-interactive go | sed 's/<>?/ --- /g')
+CHOICES=$(reflex-curses -f)
+CHOSEN=$(echo "$CHOICES" | dmenu -l 20 -i -p "Twitch Stream?") # displays channel name, current game, title, and viewers in dmenu
+CHANNEL_NAME=$(echo "$CHOSEN" | awk '{print $1}') # grabs the channel name from the selected option in dmenu
 
-#choices=$(twitchy --non-interactive go | sed 's/<>?/ --- /g' | tr -cd '\11\12\15\40-\176')
-choices=$(reflex-curses -f)
-chosen=$(echo -e "$choices" | dmenu -l 20 -i -p "Twitch Stream?") # displays channel name, current game, title, and viewers in dmenu
-channelName=$(echo "$chosen" | awk '{print $1}') # grabs the channel name from the selected option in dmenu
-
-if [ "$channelName" != "" ]
+if [ "$CHANNEL_NAME" != "" ]
 then
-	# mpv https://www.twitch.tv/"$channelName" --ytdl-format=360p
-	streamlink https://www.twitch.tv/"$channelName" 480p || \
-		streamlink https://www.twitch.tv/"$channelName" best || \
-			notify-send "Error trying to play $channelName through streamlink"
+	streamlink https://www.twitch.tv/"$CHANNEL_NAME" 480p || \
+		streamlink https://www.twitch.tv/"$CHANNEL_NAME" best || \
+			notify-send "Error trying to play $CHANNEL_NAME through streamlink"
 fi
