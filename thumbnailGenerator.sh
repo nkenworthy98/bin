d4aa87e817b4ba4026d37e2a830f089a18e03a84
Nick Kenworthy <xndt98@live.com>
2021-10-07 01:22:22 -0700

    rewrite thumbnailGenerator to be POSIX compliant

--- thumbnailGenerator.sh
+++ thumbnailGenerator.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Generate small jpg files to act as thumbnails
 # for any mp4 files returned by the find command
-set -euo pipefail
 
 find . -type f -name '*.mp4' -exec ffmpegthumbnailer -i {} -t 50% -o {}.jpg \;
