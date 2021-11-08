34d74b96bfab2272ca2e2b41b1c4b36c06890c9d
Nick Kenworthy <xndt98@live.com>
2021-11-08 01:15:45 -0800

    rewrite goto-site in perl
    This rewrite also includes the ability to enter in multiple
    usernames/subreddits in dmenu (separated by spaces) and have those pages
    opened up in a browser

---
+++ perl_scripts/goto-site.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+# Quickly navigate to nitter/teddit pages using dmenu
+#
+# When dmenu asks you to enter in a username or subreddit, you can enter in
+# multiple usernames/subreddits (separated by spaces) and have them open up
+# in a browser.
+use strict;
+use warnings;
+
+my $browser = 'firefox';
+
+my @sites_list = (
+  'nitter',
+  'teddit',
+);
+
+my $site_choice = ask_user_for_site(@sites_list);
+
+if ($site_choice eq 'nitter') {
+  open_nitter_pages();
+}
+# Using elsif in case I want to add more sites in the future
+elsif ($site_choice eq 'teddit') {
+  open_teddit_pages();
+}
+
+sub ask_user_for_site {
+  my @sites = @_;
+
+  my $site_count = @sites;
+  my $printf_format_str = create_printf_format_str($site_count);
+  my $site_choice = `printf '$printf_format_str' @sites | dmenu -i -l $site_count -p 'Site?'`;
+  chomp($site_choice);
+
+  return $site_choice;
+}
+
+# Used for the printf shell command
+# If I have 2 sites, I want the printf shell command to have:
+# '%s\n%s\n' as the formatted string
+# For 3 sites, it should be '%s\n%s\n%s\n', and so on
+sub create_printf_format_str {
+  my $string_count = shift @_;
+
+  my $printf_str = "";
+  my $count = 0;
+  while ($count < $string_count) {
+    $printf_str = $printf_str . '%s\n';
+    $count += 1;
+  }
+
+  return $printf_str;
+}
+
+sub open_nitter_pages {
+  # Open nitter home page if a username isn't provided
+  my @nitter_usernames = split(' ',`printf '' | dmenu -p 'Nitter Username(s)?'`)
+      or open_nitter_homepage();
+  my @nitter_urls;
+
+  foreach my $user (@nitter_usernames) {
+    chomp(my $nitter_base_url = `grni.sh`);
+    my $url = "https://$nitter_base_url/$user";
+
+    push(@nitter_urls, $url);
+  }
+
+  exec("$browser", @nitter_urls);
+}
+
+sub open_nitter_homepage {
+  chomp(my $nitter_home_url = `grni.sh`);
+  exec("$browser", "https://$nitter_home_url");
+}
+
+sub open_teddit_pages {
+  # Open teddit home page if a subreddit isn't provided
+  my @teddit_subreddits = split(' ',`printf '' | dmenu -p 'Teddit Subreddit(s)?'`)
+      or open_teddit_homepage();
+  my $teddit_base_url = 'teddit.net';
+  my @teddit_urls;
+
+  foreach my $subreddit (@teddit_subreddits) {
+    my $url = "https://$teddit_base_url/r/$subreddit?theme=dark";
+
+    push(@teddit_urls, $url);
+  }
+
+  exec("$browser", @teddit_urls);
+}
+
+sub open_teddit_homepage {
+  exec("$browser", "https://teddit.net?theme=dark");
+}
