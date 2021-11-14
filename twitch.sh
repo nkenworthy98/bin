13321e191e3e48e7d50f7dddad1ff3ec00e0ff60
Nick Kenworthy <xndt98@live.com>
2021-11-13 16:08:00 -0800

    allow multiple dmenu selections

--- twitch.sh
+++ twitch.sh
@@ -2,16 +2,16 @@
 # Script to choose twitch stream to watch through dmenu
 
 CHANNEL_LIST=$(reflex-curses -f)
-# Use dmenu to ask user to select a channel to watch
-SELECTED_CHANNEL=$(echo "$CHANNEL_LIST" | dmenu -l 20 -i -p "Twitch Stream?")
+# Use dmenu to ask user to select channel(s) to watch
+SELECTED_CHANNELS=$(echo "$CHANNEL_LIST" | dmenu -l 20 -i -p "Twitch Stream?")
 
 play_stream() {
 	CHANNEL=$1
 
 	# Don't even attempt to run streamlink if a channel isn't selected
-	if [ "$CHANNEL" != "" ]; then
-		streamlink https://www.twitch.tv/"$CHANNEL" || print_error "$CHANNEL"
-	fi
+	# if [ "$CHANNEL" != "" ]; then
+	streamlink https://www.twitch.tv/"$CHANNEL" || print_error "$CHANNEL"
+	# fi
 }
 
 print_error() {
@@ -20,4 +20,6 @@ print_error() {
 	notify-send 'twitch.sh' "Error trying to play $CHANNEL through streamlink"
 }
 
-play_stream "$SELECTED_CHANNEL"
+for CHANNEL in $SELECTED_CHANNELS; do
+	play_stream "$CHANNEL" &
+done
