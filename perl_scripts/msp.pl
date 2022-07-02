d2228305f9f4e4210e1072a7a81996ecf0309437
Nick Kenworthy <xndt98@live.com>
2022-07-01 21:32:31 -0700

    add --append option

--- perl_scripts/msp.pl
+++ perl_scripts/msp.pl
@@ -3,17 +3,26 @@
 use strict;
 use warnings;
 use IPC::Open2;
+use Getopt::Long qw(GetOptions HelpMessage);
 
 my $music_dir = "$ENV{'HOME'}/.Music";
+
+# CLI Flags/Variables
+my $append_flag = 0;
+
+GetOptions(
+  'append|a' => \$append_flag,
+  'help|h' => sub { HelpMessage(0) },
+) or HelpMessage(1);
 
 my @songs = grep { ! /\.git/ } `mpc listall`;
 my $dmenu_str = join('', @songs);
 my @selections = send_to_dmenu($dmenu_str);
 
 if (@selections) {
- system("mpc clear");
+ system("mpc clear") unless $append_flag;
  system("mpc", "add", @selections);
- system("mpc play");
+ system("mpc play") unless $append_flag;
 }
 
 sub send_to_dmenu {
@@ -29,3 +38,18 @@ sub send_to_dmenu {
 
   return @dmenu_selections;
 }
+
+=head1 NAME
+
+msp.pl - mpc search play
+
+=head1 SYNOPSIS
+
+msp.pl [OPTION]
+
+  -a, --append     Append selection(s) to current queue
+  -h, --help       Print this help and exit
+
+For more detailed documentation, run C<perldoc msp.pl>
+
+=cut
