6d451e7e850792876f315505c632d02f5920313a
Nick Kenworthy <xndt98@live.com>
2023-02-13 20:31:52 -0800

    create and switch to separate tmux session

--- perl_scripts/ncmpcpp-preview.pl
+++ perl_scripts/ncmpcpp-preview.pl
@@ -5,10 +5,17 @@
 use strict;
 use warnings;
 
+my $session_name = 'ncmppcp-preview';
+
+if (grep { /^$session_name:/ } `tmux ls`) {
+  system("tmux kill-session -t '$session_name'");
+}
+
+my $music_dir = "$ENV{HOME}/.Music";
 my $relative_music_path = `mpc status -f '%file%' | head -n 1`;
 # Newline at end can cause issues, so remove it
 chomp($relative_music_path);
-my $full_music_path = "$ENV{HOME}/.Music/$relative_music_path";
+my $full_music_path = "$music_dir/$relative_music_path";
 
 # If '%file%' is a cue file ending in with track, substitute it out
 # Without this, this program won't be able to navigate to the proper directory
@@ -22,7 +29,5 @@ if ($full_music_path =~ /\.cue(?<cue_track>\/track\d+\Z)/) {
 }
 
 # 'p' is the plugin key I have for nnn in order to use preview-tui
-system("nnn", "-P", "p", "$full_music_path");
-
-# Useful for debugging
-# print "$full_music_path\n";
+system(qq(tmux new-session -s $session_name -d nnn -P p "$full_music_path"));
+system("tmux switch-client -t '$session_name'");
