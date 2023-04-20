cc66ea3899bb843dd231a8c5ed3ec2920fe539b6
Nick Kenworthy <xndt98@live.com>
2023-04-19 22:58:56 -0700

    forget the colors for now

--- perl_scripts/tmux-search-buffer.pl
+++ perl_scripts/tmux-search-buffer.pl
@@ -2,7 +2,6 @@
 use strict;
 use warnings;
 use Getopt::Long qw(GetOptions HelpMessage);
-use Term::ANSIColor qw(colored);
 
 my $tmp_file = '/tmp/tmux-search-buffer.txt';
 my $tmux_active_regex = qr{^(\d+):.*?\(active\)};
@@ -16,28 +15,6 @@ GetOptions(
     },
 ) or HelpMessage(1);
 
-# my @windows_list = `tmux list-windows`;
-# chomp(@windows_list);
-# my $active_window;
-# foreach my $window_line (@windows_list) {
-#     if ($window_line =~ $tmux_active_regex) {
-#         $active_window = parse_number_at_line_start($window_line);
-#         last;
-#     }
-# }
-
-# my @panes_list = `tmux list-panes -t $active_window`;
-# chomp(@panes_list);
-# my $active_pane;
-# foreach my $pane_line (@panes_list) {
-#     if ($pane_line =~ $tmux_active_regex) {
-#         $active_pane = parse_number_at_line_start($pane_line);
-#         last;
-#     }
-# }
-
-# my $window_and_pane = "$active_window.$active_pane";
-# system("tmux capture-pane -t $window_and_pane -S - -J -p > $tmp_file");
 system("tmux capture-pane -S - -J -p > $tmp_file");
 system("tmux new-window -n search");
 system("tmux send-keys 'tmux-search-buffer.pl --prompt' Enter\;");
@@ -65,18 +42,9 @@ sub prompt_and_filter_lines {
         or die "Error when opening $file: $!";
 
     while (my $line = <$fh>) {
-        # next if $line =~ /[\[\]]/;
         chomp($line);
         if ($line =~ /($regex)/i) {
-            my $colored_regex = colored($1, "bold red");
-            # print "regex: '$regex'\n";
-            # print "match: '$1'\n";
-            # print "color: $colored_regex\n";
-            # exit;
-            # my $colored_line = $line =~ s/$1/$colored_regex/gr;
-            # my $colored_line = $line =~ s/\Q$1\E/$colored_regex/gr;
-            my $colored_line = $line =~ s/\Q$1\E/$colored_regex/gr;
-            push(@matching_lines, $colored_line);
+            push(@matching_lines, $line);
         }
     }
 
