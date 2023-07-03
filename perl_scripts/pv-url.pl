30ba6b7ec35b94862e912bbab32c2a290c158722
Nick Kenworthy <xndt98@live.com>
2023-07-02 20:37:01 -0700

    parse newsboat tmux pane for invidious link

--- perl_scripts/pv-url.pl
+++ perl_scripts/pv-url.pl
@@ -1,13 +1,28 @@
 #!/usr/bin/perl
+# Parse youtube/invidious url from tmux pane and open comments in another winodw
+# using pipe-viewer
 use strict;
 use warnings;
-use List::Util qw(first);
 
 my @tmux_pane_contents = `tmux capture-pane -p`;
-my $vid_url = first { $_ }
-              map { m{^=> URL: (https://.*?)\s+$} ? ($1) : () }
-              reverse
-              @tmux_pane_contents;
+chomp(@tmux_pane_contents);
+
+my $vid_url;
+foreach my $line (reverse @tmux_pane_contents) {
+    # url from pipe-viewer
+    if ($line =~ m{^=> URL: (https://.*?)\s*$}) {
+        my $invidious_instance = `grii.sh`;
+        chomp($invidious_instance);
+        my $invidious_url = $1 =~ s/www\.youtube\.com/$invidious_instance/r;
+        $vid_url = $invidious_url;
+        last;
+    }
+    # invidious url from rss feed in newsboat
+    elsif ($line =~ m{^\[1\]: (https://.*?)\s\(link\)\s*$}) {
+        $vid_url = $1;
+        last;
+    }
+}
 
 if ($vid_url) {
     system("tmux new-window -n comments");
